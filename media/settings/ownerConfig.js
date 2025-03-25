7c556aafacef3cf2f76b3c2f8ce88145bee38264
gravywrd <graveconsol@gmail.com>
2025-03-25 21:45:14 +0700

    Create ownerConfig.js

---
+++ media/settings/ownerConfig.js
@@ -0,0 +1,13 @@
+
+const fs = require('fs')
+// gausah di ubah rek karna
+// udh ada request pas di scan pairing
+global.ownername = 'Grave';
+global.owner = '6283804078729';
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    delete require.cache[file]
+    require(file)
+})
